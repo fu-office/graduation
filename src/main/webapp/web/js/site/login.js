46b26629834716150db382b0e4dfb304c4929ee7
zhenglianfu <15067134439@163.com>
2015-04-07 18:06:42 +0800

    update

--- src/main/webapp/web/js/site/login.js
+++ src/main/webapp/web/js/site/login.js
@@ -2,7 +2,11 @@
 	$('#regist_btn').bind('click', function(){
 		var data = {};
 		$('#login_form input').each(function(){
-			
+			var $ipt = $(this);
+			data[this.name] = $ipt.val();
+			if ($ipt.hasClass('required') && !$ipt.val()) {
+				$ipt.addClass('error');
+			}
 		});
 	});
 }());
